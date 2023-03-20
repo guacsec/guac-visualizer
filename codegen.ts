7f3be60adab57102ceaf963a514a31b1fb1fcedf
Michael Lieberman <mlieberman85@gmail.com>
2023-03-20 09:48:13 -0400

    Add codegen for graphql schema
    Signed-off-by: Michael Lieberman <mlieberman85@gmail.com>

--- codegen.ts
+++ codegen.ts
@@ -2,10 +2,12 @@
 import type { CodegenConfig } from '@graphql-codegen/cli';
 
 const config: CodegenConfig = {
+  // TODO (mlieberman85): Below should change to some API endpoint with the schema or to soem place to download the schema.
+  // For now this is just the relative path to my local clone of guac.
   schema: '../guac/pkg/assembler/graphql/**/*.graphql',
-  documents: ['src/**/*.tsx'],
+  documents: ['gql/**/*.tsx'],
   generates: {
-    './src/__generated__/': {
+    './gql/__generated__/': {
       preset: 'client',
       plugins: [],
       presetConfig: {
