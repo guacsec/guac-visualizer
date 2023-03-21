6ef48c709183f9ce5a4293cf34ceb914e838616c
Brandon Lum <lumjjb@gmail.com>
2023-03-21 14:02:49 -0400

    fix codgen for real
    Signed-off-by: Brandon Lum <lumjjb@gmail.com>

--- codegen.ts
+++ codegen.ts
@@ -5,24 +5,19 @@ const config: CodegenConfig = {
   // TODO (mlieberman85): Below should change to some API endpoint with the schema or to soem place to download the schema.
   // For now this is just the relative path to my local clone of guac.
   schema: [
-     '../guac/pkg/assembler/graphql/**/*.graphql',
-     '../guac/pkg/assembler/graphql/examples/*.gql',
+     '../guac/pkg/assembler/graphql/schema/*.graphql',
   ],
-
-  documents: ['gql/**/*.tsx', '../guac/pkg/assembler/graphql/examples/*.gql'],
+  documents: ['../guac/pkg/assembler/graphql/examples/*.gql'],
   generates: {
     './gql/__generated__/': {
       preset: 'client',
       plugins: [
-        "typescript-react-apollo",
-        "typescript-operations",
       ],
       presetConfig: {
         gqlTagName: 'gql',
-      }
+      },
     }
   },
-  ignoreNoDocuments: true,
 };
 
 export default config;
