384b0b273b04a62240b9c48ccbbbb3ba9201f702
Brandon Lum <lumjjb@gmail.com>
2023-03-22 16:45:56 -0400

    add functionality to graph
    Signed-off-by: Brandon Lum <lumjjb@gmail.com>

--- app/graph.tsx
+++ app/graph.tsx
@@ -3,9 +3,13 @@
 import CytoscapeComponent from 'react-cytoscapejs'
 import Spread from 'cytoscape-spread';
 import COSEBilkent from 'cytoscape-cose-bilkent'
-import { useState } from 'react';
-import { EventObject } from 'cytoscape';
+import { useState, useEffect } from 'react';
+import cytoscape, { EventObject } from 'cytoscape';
 import Cytoscape from 'cytoscape';
+import { randomUUID } from 'crypto';
+import { IsDependency,  GetPkgQuery, GetPkgQueryVariables, PkgSpec , GetPkgDocument, AllPkgTreeFragment} from '../gql/__generated__/graphql';
+import { gql, useQuery, useLazyQuery } from '@apollo/client';
+
 
 Cytoscape.use(Spread);
 Cytoscape.use(COSEBilkent);
@@ -32,10 +36,76 @@ type GraphData = {
 }
 
 type GraphProps = {
-  graphData?: GraphData;
+  //graphData?: GraphData;
   layout?: string;
+
   writeDetails?: (x: any) => void;
 };
+
+class GuacNode {
+  nodeId: number;
+  nodeType: string;
+  data: undefined;
+
+  constructor(nodeData: any) {
+
+    // parse graphql data and determine what the node id and nodeType is and put the entire dict in data for JSON output
+    this.nodeId = nodeData.nodeId;
+    this.nodeType = "something";
+    this.data = nodeData;
+  }
+}
+
+type NodeId = number;
+
+class GuacGraphHelper {
+  frontier: GuacNode[];
+  nodeMap: Map<number,GuacNode>;
+  expandedMap: Map<number,boolean>;
+
+  constructor(message: string) {
+    this.frontier = [];
+    this.nodeMap = new Map<NodeId, GuacNode>;
+    this.expandedMap = new Map<NodeId, boolean>;
+  }
+  // populateData will take a node and figure out the other nodes and edges to create and
+  // - update the frontier by removing itself and adding the new nodes
+  // - adding the new nodes to the nodeMap
+  // - adding the new nodes and edges to cytoscape graph
+  private populateData (node:GuacNode) {
+
+  }
+
+  public expandNode(id: NodeId) {
+    const n = this.getNodeEdges(id);
+    // check if n is null and populate if it is not.
+    this.populateData(n);
+  }
+
+  public getNodeEdges(id: NodeId) : GuacNode {
+    // check if it is in expnaded map, if its not been expanded, query for node (with its edge properties), else return null
+    return new GuacNode({});
+  }
+
+  public expandFrontier() {
+    const currentFrontier = Object.assign([], this.frontier);
+    currentFrontier.forEach((n)=>
+      this.expandNode(n)
+    );
+  }
+}
+
+
+function guacToCy(n:GuacNode):  Node {
+  return {
+    data : {
+      id: n.nodeId.toString(),
+      label: n.nodeType,
+      type: n.nodeType,
+    }
+  }
+}
+
 
 
 const defaultGraphData: GraphData = {
@@ -82,13 +152,25 @@ const defaultGraphData: GraphData = {
   ]
 };
 
+let refCy :cytoscape.Core;
 export default function Graph(props: GraphProps) {
+  
+
   const [width, setWidth] = useState("100%");
   const [height, setHeight] = useState("800px");
-  console.log(props.graphData)
+  const [graphDesiredState, setGraphDesiredState] = useState([1,2,3,4,5,6,7,8,9,10,11,12]);
+  const [neighborsDesired, setNeighborsDesired] = useState([]);
+  const [gotNeighbors, setGotNeighbors] = useState(new Set<number>());
+
+
 
   const [graphData, setGraphData] = useState(props?.graphData ?? defaultGraphData);
 
+
+  
+
+
+  //console.log(props.graphData)
   const layout = {
     name: props?.layout ?? "breadthfirst",
     fit: true,
@@ -166,22 +248,83 @@ export default function Graph(props: GraphProps) {
       }
     }
   ];
+  
+  for (let i=graphDesiredState.length-1;i>=0;  i--) {
+    console.log(refCy);
+    console.log("gds loop" + graphDesiredState[i]);
+    console.log(graphDesiredState);
+    
+    const found =  graphData.nodes.find((a)=>a.data.id  == graphDesiredState[i].toString());
+    //console.log(graphDesiredState);
+    //console.log(graphData.nodes);
+    //if (found != undefined) {
+    //  console.log("updated all existing nodes");
+    //  break;
+    //}
+
+    if (refCy != undefined) {
+      if (!refCy.hasElementWithId(graphDesiredState[i].toString())) {
+        refCy.add({data: { id: graphDesiredState[i].toString(), label: "node-" + graphDesiredState[i].toString()}});
+        refCy.layout(layout).run();
+        /*
+        const { data, error } = useQuery(GetPkgDocument,{
+          variables: {
+            spec: JSON.parse(`{
+              "type":"deb",
+              "namespace":"ubuntu",
+              "name": "dpkg",
+              "qualifiers": [{"key":"arch", "value":"amd64"}]
+            }`),
+          }
+        })
+        console.log(data);
+        */
+      } else {
+        console.log("updated all existing nodes");
+        break;
+      }
+    } else {
+      console.log("cy not initialized yet");
+    }
+    //graphData.nodes = [...graphData.nodes, {data: { id: graphDesiredState[i].toString(), label: "node-" + graphDesiredState[i].toString()}}];
+    //setGraphData(graphData);
+
+  }
 
   let nodeTapHandler = (evt: EventObject) => {
     var node = evt.target;
     // TODO: This should potentially run additional queries that then update the component state
     console.log("EVT", evt);
-    console.log("TARGET", node.data());
-    console.log("TARGET TYPE", typeof node[0]);
+    let rnd = Math.floor(Math.random() * (1000 - 100 + 1) + 100);
+    console.log(graphDesiredState);
+    setGraphDesiredState([...graphDesiredState, rnd]);
+    
     if (props.writeDetails != undefined) {
       props.writeDetails(node.data());
-    }
+    }    
+  }
+
+  let nodeCxttapHandler = (evt: EventObject) => {
+    var node = evt.target;
+    // TODO: This should potentially run additional queries that then update the component state
+    console.log("EVT", evt);
+  }
+
+
+  let addNode = () => {
+    console.log("add innode");
+    console.log(graphData);
+    console.log(refCy);
+    console.log(refCy.json());
+    //refCy.add({ data: { id: "abc",label: "FJIOEJIOWFJEIOWJFOEIWFJEIOWJFEWOFIJEIOF 10", type: "fewafawefawef" } });
+    //graphData.nodes.push({ data: { id: "abc",label: "FJIOEJIOWFJEIOWJFOEIWFJEIOWJFEWOFIJEIOF 10", type: "fewafawefawef" } });
+    //setGraphData(graphData);
     
   }
 
-  let ref;
-
   return (
+    <>
+    <button onClick={addNode}> add node </button>
     <CytoscapeComponent
       elements={CytoscapeComponent.normalizeElements(graphData)}
       style={{ width: width, height: height }}
@@ -193,11 +336,13 @@ export default function Graph(props: GraphProps) {
       layout={layout}
       stylesheet={styleSheet}
       cy={cy => {
-        ref = cy;
+        refCy = cy;
         console.log("EVT", cy);
         cy.on("tap", "node", evt => nodeTapHandler(evt));
+        cy.on("cxttap", "node", evt => nodeCxttapHandler(evt));
       }}
     />
+    </>
   )
 }
 
