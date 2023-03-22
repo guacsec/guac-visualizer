b6cda6c4136373bf5bcf4ad90d304056da6d3ec8
Brandon Lum <lumjjb@gmail.com>
2023-03-22 16:45:56 -0400

    add functionality to graph
    Signed-off-by: Brandon Lum <lumjjb@gmail.com>

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,13 @@
 import { Inter } from '@next/font/google'
 import React, { useState } from "react";
 import Graph from 'app/graph'
-import { gql, useQuery } from '@apollo/client'
+import { gql, useQuery, useLazyQuery } from '@apollo/client'
 import styles from "../styles/Home.module.css";
 import useSWR from 'swr'
 import client from 'apollo/client'
 import { Node, Edge } from 'app/graph'
 import SBOMViewer from '@/app/sbom';
-
+import { IsDependency,  GetPkgQuery, GetPkgQueryVariables, PkgSpec , GetPkgDocument, AllPkgTreeFragment} from '../gql/__generated__/graphql';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -63,6 +63,9 @@ query Q1 {
 }`;
 
 const fetcher = async (query) => {
+  
+
+  
   const { data } = await client.query({
     query: query,
     variables: {},
@@ -79,17 +82,25 @@ const processDataForCytoscape = (data) => {
   const nodes: Node[] = [];
   const edges: Edge[] = [];
 
-  data.IsDependency.forEach((dependency, index) => {
+  data.IsDependency.forEach((dependency :IsDependency, index) => {
     const sourceNodeId = `pkg-${index}`;
     const targetNodeId = `depPkg-${index}`;
 
     // Create nodes for package and dependentPackage
     nodes.push(
       {
-        data: { id: sourceNodeId, label: dependency.package.namespaces[0].names[0].name },
+        data: { 
+          id: sourceNodeId, 
+          label: dependency.package.namespaces[0].names[0].name, 
+          type: dependency.__typename ?? "",
+        },
       },
       {
-        data: { id: targetNodeId, label: dependency.dependentPackage.namespaces[0].names[0].name },
+        data: { 
+          id: targetNodeId, 
+          label: dependency.dependentPackage.namespaces[0].names[0].name,
+          type: dependency.__typename ?? "",
+        },
       }
     );
 
@@ -110,19 +121,47 @@ const processDataForCytoscape = (data) => {
   return { nodes, edges };
 };
 
+
 export default function Home() {
   // TODO (mlieberman85): Validate if SWR is better in this use case than alternatives like react query
   const [detailsText, setDetailsText] = useState("detailsText");
+  const [inputText, setInputText] = useState(`{
+    "type":"deb",
+    "namespace":"ubuntu",
+    "name": "dpkg",
+    "qualifiers": [{"key":"arch", "value":"amd64"}]
+  }`);
+  const [buttonHit, setButtonHit] = useState(false);
+  const [requested, setRequested] = useState("{}");
+  const [updated, setUpdated] = useState("");
+
+
+
+
   function writeDetailsHandler (x : any) {
     setDetailsText(JSON.stringify(x,null,2));
+
   }
-  const { data, error } = useSWR(TEST_QUERY, fetcher)
-  if (error) return <div>failed to load</div>
-  if (!data) return <div>loading...</div>
+
+    const { data, error } = useQuery(GetPkgDocument,{
+      variables: {
+        spec: JSON.parse(requested),
+      }
+    })
+    console.log(data);
+  
+
+  //const { data, error } = useSWR(TEST_QUERY, fetcher)
+  //if (error) return <div>failed to load</div>
+  //if (!data) return <div>loading...</div>
   return (
     <>
       <div>
         <h1>GUAC Visualizer</h1>
+        <textarea name="text" rows={15} cols={50} value={inputText} onChange={e => setInputText(e.target.value)} />
+        <button onClick={e => setRequested(inputText)}>submit</button>
+        <textarea name="details-text" rows={15} cols={50} value={JSON.stringify(data)} />
+
         <textarea name="details-text" rows={15} cols={50} value={detailsText} onChange={e => setDetailsText(e.target.value)}/>
         <div
           style={{
@@ -138,4 +177,3 @@ export default function Home() {
     </>
   )
 }
-
