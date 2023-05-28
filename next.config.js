c53e9e55d5b5007f37f7df5b5a12629003ca66af
shaf <shafeelinks@gmail.com>
2023-05-28 17:50:08 -0400

    update guac config to accept root guac.yaml
    Signed-off-by: shaf <shafeelinks@gmail.com>

--- next.config.js
+++ next.config.js
@@ -1,21 +1,21 @@
 const fs = require('fs');
 const yaml = require('js-yaml');
+const path = require('path');
 
 // if a gql endpoint isn't defined, default to localhost
 const GUAC_GQL_ADDR = process.env.GUAC_GQL_ADDR || 'http://localhost:8080/query';
-const GUAC_CONFIG_PATH = process.env.GUAC_CONFIG_PATH;
+// default to 'guac.yaml' in the same directory as this file
+const GUAC_CONFIG_PATH = process.env.GUAC_CONFIG_PATH || path.resolve(__dirname, 'guac.yaml');
 
 let gqlEndpoint = GUAC_GQL_ADDR;
 
-// if there is a config file path defined, read it
-if (GUAC_CONFIG_PATH) {
-  try {
-    let fileContents = fs.readFileSync(GUAC_CONFIG_PATH, 'utf8');
-    let guacConfig = yaml.load(fileContents);
-    gqlEndpoint = guacConfig['gql-addr'] || GUAC_GQL_ADDR;
-  } catch (err) {
-    console.error('error reading guac.yaml file : ', err);
-  }
+// try to read the config file
+try {
+  let fileContents = fs.readFileSync(GUAC_CONFIG_PATH, 'utf8');
+  let guacConfig = yaml.load(fileContents);
+  gqlEndpoint = guacConfig['gql-addr'] || GUAC_GQL_ADDR;
+} catch (err) {
+  console.error(`Error reading guac.yaml file at ${GUAC_CONFIG_PATH}: `, err);
 }
 
 /** @type {import('next').NextConfig} */
