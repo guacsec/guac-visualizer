9e797c6239d18e2be2b829bde3efe3ba5487d8fb
lillichoung <35208156+lillichoung@users.noreply.github.com>
2023-06-06 14:04:36 -0400

    Clean up
    Signed-off-by: lillichoung <35208156+lillichoung@users.noreply.github.com>

--- apollo/client.ts
+++ apollo/client.ts
@@ -1,66 +1,70 @@
-import { useMemo } from 'react'
-import { ApolloClient, InMemoryCache, NormalizedCacheObject } from '@apollo/client'
-import merge from 'deepmerge'
+import { useMemo } from "react";
+import {
+  ApolloClient,
+  InMemoryCache,
+  NormalizedCacheObject,
+} from "@apollo/client";
+import merge from "deepmerge";
 
-let apolloClient: ApolloClient<NormalizedCacheObject>
+let apolloClient: ApolloClient<NormalizedCacheObject>;
 
 function createApolloClient() {
   return new ApolloClient({
-    ssrMode: typeof window === 'undefined',
+    ssrMode: typeof window === "undefined",
     // link: createIsomorphLink(),
     uri: "http://localhost:3000/api/graphql",
     cache: new InMemoryCache(),
     defaultOptions: {
       watchQuery: {
-        fetchPolicy: 'no-cache',
+        fetchPolicy: "no-cache",
       },
       query: {
-        fetchPolicy: 'no-cache',
-      }
-    }
-  })
+        fetchPolicy: "no-cache",
+      },
+    },
+  });
 }
 
 const client = new ApolloClient({
-    ssrMode: typeof window === 'undefined',
-    // link: createIsomorphLink(),
-    uri: "http://localhost:3000/api/graphql",
-    credentials: "same-origin",
-    cache: new InMemoryCache(),
-    defaultOptions: {
-      watchQuery: {
-        fetchPolicy: 'no-cache',
-      },
-      query: {
-        fetchPolicy: 'no-cache',
-      }
-    }
-  })
+  ssrMode: typeof window === "undefined",
+  // link: createIsomorphLink(),
+  uri: "http://localhost:3000/api/graphql",
+  credentials: "same-origin",
+  cache: new InMemoryCache(),
+  defaultOptions: {
+    watchQuery: {
+      fetchPolicy: "no-cache",
+    },
+    query: {
+      fetchPolicy: "no-cache",
+    },
+  },
+});
 
-export function initializeApollo(initialState = null) {
-  const _apolloClient = apolloClient ?? createApolloClient()
+export function initializeApollo(initialState: NormalizedCacheObject = null) {
+  const _apolloClient = apolloClient ?? createApolloClient();
 
   if (initialState) {
     // Get existing cache, loaded during client side data fetching
-    const existingCache = _apolloClient.extract()
+    const existingCache = _apolloClient.extract();
 
     // Merge the existing cache into data passed from getStaticProps/getServerSideProps
-    const data = merge(initialState, existingCache)
+    const data = merge(initialState, existingCache);
 
     // Restore the cache with the merged data
-    _apolloClient.cache.restore(data)
+    _apolloClient.cache.restore(data);
   }
 
-  if (typeof window === 'undefined') return _apolloClient
+  if (typeof window === "undefined") return _apolloClient;
 
-  if (!apolloClient) apolloClient = _apolloClient
+  if (!apolloClient) apolloClient = _apolloClient;
 
-  return _apolloClient
+  return _apolloClient;
 }
 
-export function useApollo(initialState) {
-  const store = useMemo(() => initializeApollo(initialState), [initialState])
-  return store
+export function useApollo(initialState: NormalizedCacheObject) {
+  const store = useMemo(() => initializeApollo(initialState), [initialState]);
+  return store;
 }
 
-export default client
+export default client;
