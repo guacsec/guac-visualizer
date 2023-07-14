05900911b6ab9a1496400af9d44f9649888068e1
shafeeshafee <shafeelinks@gmail.com>
2023-07-14 15:31:27 -0400

    change codegen documents to use client instead of examples
    Signed-off-by: shafeeshafee <shafeelinks@gmail.com>

--- codegen.ts
+++ codegen.ts
@@ -1,5 +1,4 @@
-
-import type { CodegenConfig } from '@graphql-codegen/cli';
+import type { CodegenConfig } from "@graphql-codegen/cli";
 
 const config: CodegenConfig = {
   // TODO (mlieberman85): Below should change to some API endpoint with the schema or to soem place to download the schema.
@@ -9,14 +8,13 @@ const config: CodegenConfig = {
   ],
   documents: ['../guac/pkg/assembler/clients/operations/*.graphql'],
   generates: {
-    './gql/__generated__/': {
-      preset: 'client',
-      plugins: [
-      ],
+    "./gql/__generated__/": {
+      preset: "client",
+      plugins: [],
       presetConfig: {
-        gqlTagName: 'gql',
+        gqlTagName: "gql",
       },
-    }
+    },
   },
 };
 
