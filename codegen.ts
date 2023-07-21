9081a4e1f347ad87a79b53c10865d2fadf646871
shafeeshafee <shafeelinks@gmail.com>
2023-07-21 16:22:06 -0400

    Revert "Merge pull request #43 from shafeeshafee/codegen-documents"
    This reverts commit e265e4f04a597a5f995ae25466f30fc6993dce7a, reversing
    changes made to 96570e6024134409bcf7b7e3cb7348baf504fe06.
    
    Signed-off-by: shafeeshafee <shafeelinks@gmail.com>

--- codegen.ts
+++ codegen.ts
@@ -1,18 +1,22 @@
-import type { CodegenConfig } from "@graphql-codegen/cli";
+
+import type { CodegenConfig } from '@graphql-codegen/cli';
 
 const config: CodegenConfig = {
   // TODO (mlieberman85): Below should change to some API endpoint with the schema or to soem place to download the schema.
   // For now this is just the relative path to my local clone of guac.
-  schema: ["../guac/pkg/assembler/graphql/schema/*.graphql"],
-  documents: ["../guac/pkg/assembler/graphql/generated/*.gql"],
+  schema: [
+     '../guac/pkg/assembler/graphql/schema/*.graphql',
+  ],
+  documents: ['../guac/pkg/assembler/graphql/examples/*.gql'],
   generates: {
-    "./gql/__generated__/": {
-      preset: "client",
-      plugins: [],
+    './gql/__generated__/': {
+      preset: 'client',
+      plugins: [
+      ],
       presetConfig: {
-        gqlTagName: "gql",
+        gqlTagName: 'gql',
       },
-    },
+    }
   },
 };
 
