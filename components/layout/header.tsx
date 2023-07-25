80e2bd1c3acfe099676329a441db6a23f5273230
shaf <shafeelinks@gmail.com>
2023-07-25 10:55:07 -0400

    fix <Link> usage in Header and add useEffect dependency in index
    Signed-off-by: shaf <shafeelinks@gmail.com>

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import { MoonIcon, SunIcon, Bars3Icon } from "@heroicons/react/24/solid";
 import GuacVizThemeContext from "@/store/themeContext";
 import NavigationLinks from "./navlinks";
+import Link from "next/link";
 
 export default function Header() {
   const { isDarkTheme, toggleThemeHandler } = useContext(GuacVizThemeContext);
@@ -24,22 +25,21 @@ export default function Header() {
     <>
       <div className="flex justify-between bg-stone-200 dark:bg-stone-800 text-zinc-600 px-4 md:px-8 py-4 items-center backdrop-blur-sm w-full font-mono transition-colors duration-500">
         <div className="flex items-center">
-          <a
-            className="flex items-center hover:text-stone-500 dark:hover:text-stone-400 transition-colors duration-500"
-            href="/"
-          >
-            <Image
-              className="dark:white-filter transition-all duration-500"
-              src="images/icons/guac-logo.svg"
-              alt="GUAC Logo"
-              width={27}
-              height={27}
-            />
-            <h1 className="ml-2 text-2xl dark:text-stone-300 transition-all duration-500">
-              GUAC Visualizer
-            </h1>
-            <span className="text-gray-400 ml-3"> *Experimental </span>
-          </a>
+          <Link href="/">
+            <div className="flex items-center hover:text-stone-500 dark:hover:text-stone-400 transition-colors duration-500">
+              <Image
+                className="dark:white-filter transition-all duration-500"
+                src="images/icons/guac-logo.svg"
+                alt="GUAC Logo"
+                width={27}
+                height={27}
+              />
+              <h1 className="ml-2 text-2xl dark:text-stone-300 transition-all duration-500">
+                GUAC Visualizer
+              </h1>
+              <span className="text-gray-400 ml-3"> *Experimental </span>
+            </div>
+          </Link>
         </div>
         <div className="hidden md:flex w-1/3 flex-wrap justify-between items-center">
           <div className="flex">
