c1066bb5829e9486ec8c744adc3ee6fa4d818ba2
shaf <68356865+shafeeshafee@users.noreply.github.com>
2023-07-26 15:14:13 -0400

    create tooltip structure for right click
    Signed-off-by: shaf <68356865+shafeeshafee@users.noreply.github.com>

--- app/ForceGraph2D.tsx
+++ app/ForceGraph2D.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useCallback, useRef } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import ForceGraph, {
   LinkObject,
   ForceGraphMethods,
@@ -56,24 +56,18 @@ const ForceGraph2D: React.FC<ForceGraph2DWrapperProps & ResponsiveProps> = ({
   height,
   bgdColor,
 }) => {
+  const [tooltipStyle, setTooltipStyle] = useState({
+    display: "none",
+    top: 0,
+    left: 0,
+  });
+  const [tooltipContent, setTooltipContent] = useState("");
+
   const fgRef = useRef<ForceGraphMethods>();
 
   const handleClick = useCallback(
     (node: NodeObject) => {
       dataFetcher(node.id);
-      const distance = 200;
-      // const distRatio = 1 + distance / Math.hypot(node.x, node.y, node.z);
-      /*if (fgRef.current) {
-                fgRef.current.cameraPosition(
-                    {
-                        x: node.x * distRatio,
-                        y: node.y * distRatio,
-                        z: node.z * distRatio
-                    },
-                    node,
-                    1000
-                );
-            }*/
     },
     [fgRef, dataFetcher]
   );
@@ -81,48 +75,75 @@ const ForceGraph2D: React.FC<ForceGraph2DWrapperProps & ResponsiveProps> = ({
   if (selectedNode) {
     const sn = graphData.nodes.find((node) => node.id === selectedNode.value);
     if (sn && fgRef.current) {
-      const distance = 200;
-      // const distRatio = 1 + distance / Math.hypot(sn.x, sn.y, sn.z);
-
-      // fgRef.current.cameraPosition(
-      //     {
-      //         x: sn.x * distRatio,
-      //         //y: sn.y * distRatio,
-      //         z: sn.z * distRatio,
-      //     },
-      //     sn,
-      //     1000
-      // );
     }
   }
 
+  useEffect(() => {
+    const hideTooltip = () => {
+      setTooltipStyle((prev) => ({ ...prev, display: "none" }));
+    };
+
+    document.addEventListener("click", hideTooltip);
+    return () => document.removeEventListener("click", hideTooltip);
+  }, []);
+
   return (
-    <ForceGraph
-      backgroundColor={bgdColor}
-      width={width}
-      height={height}
-      ref={fgRef}
-      graphData={graphData}
-      nodeAutoColorBy={nodeAutoColorBy}
-      linkDirectionalArrowLength={linkDirectionalArrowLength}
-      linkDirectionalArrowRelPos={linkDirectionalArrowRelPos}
-      onNodeClick={onNodeClick ?? handleClick}
-      nodeLabel={nodeLabel}
-      linkLabel={linkLabel}
-      nodeId="id"
-      linkDirectionalParticles={linkDirectionalParticles}
-      linkDirectionalParticleWidth={0.5}
-      linkSource={linkSource}
-      linkTarget={linkTarget}
-      linkWidth={1}
-      //numDimensions={3}
-      cooldownTicks={100}
-      cooldownTime={15000}
-      nodeCanvasObjectMode={nodeCanvasObjectMode}
-      nodeCanvasObject={nodeCanvasObject}
-      onNodeDragEnd={onNodeDragEnd}
-      //nodeThreeObject={nodeThreeObject}
-    />
+    <>
+      <div
+        className="absolute font-mono bg-white p-2 rounded text-sm text-black shadow-lg border-2 border-black"
+        style={{
+          ...tooltipStyle,
+          width: "200px",
+          height: "200px",
+          zIndex: 10000,
+        }}
+      >
+        <button
+          onClick={(e) => {
+            e.stopPropagation();
+            setTooltipStyle((prev) => ({ ...prev, display: "none" }));
+          }}
+          className="absolute right-1 top-1 bg-red-400 px-2 py-1 text-white"
+        >
+          X
+        </button>
+        <p className="pt-3">{tooltipContent}</p>
+      </div>
+      <ForceGraph
+        backgroundColor={bgdColor}
+        width={width}
+        height={height}
+        ref={fgRef}
+        graphData={graphData}
+        nodeAutoColorBy={nodeAutoColorBy}
+        linkDirectionalArrowLength={linkDirectionalArrowLength}
+        linkDirectionalArrowRelPos={linkDirectionalArrowRelPos}
+        onNodeClick={onNodeClick ?? handleClick}
+        onNodeRightClick={(node: NodeObject, event: MouseEvent) => {
+          event.preventDefault();
+          console.log(`Right clicked on node with id: ${node.id}`);
+          setTooltipStyle({
+            display: "block",
+            top: event.clientY,
+            left: event.clientX,
+          });
+          setTooltipContent(`Node ID: ${node.id}`);
+        }}
+        nodeLabel={nodeLabel}
+        linkLabel={linkLabel}
+        nodeId="id"
+        linkDirectionalParticles={linkDirectionalParticles}
+        linkDirectionalParticleWidth={0.5}
+        linkSource={linkSource}
+        linkTarget={linkTarget}
+        linkWidth={1}
+        cooldownTicks={100}
+        cooldownTime={15000}
+        nodeCanvasObjectMode={nodeCanvasObjectMode}
+        nodeCanvasObject={nodeCanvasObject}
+        onNodeDragEnd={onNodeDragEnd}
+      />
+    </>
   );
 };
 
