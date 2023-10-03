3f7d8fd742faa70949747e773ab625a291492ac6
shafeeshafee <shafeelinks@gmail.com>
2023-10-03 19:29:17 -0400

    make layout serve on server instead of client
    Signed-off-by: shafeeshafee <shafeelinks@gmail.com>

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,12 @@
-"use client";
-
-import { GuacVizThemeContextProvider } from "@/store/themeContext";
 import "@/styles/globals.css";
 import Footer from "@/components/layout/footer";
 import Header from "@/components/layout/header";
-import { ApolloProvider } from "@apollo/client";
-import client from "@/apollo/client";
+
+export const metadata = {
+  title: "GUAC Visualizer",
+  description:
+    "GUAC Visualizer is an experimental utility that can be used to visualized data loaded from GUAC.",
+};
 
 export default function RootLayout({
   children,
@@ -13,20 +14,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <ApolloProvider client={client}>
-      <html>
-        <body>
-          <GuacVizThemeContextProvider>
-            <div className="flex flex-col h-screen">
-              <Header />
-              <main className="flex-grow bg-zinc-100 dark:bg-zinc-700 text-black dark:text-zinc-50">
-                {children}
-              </main>
-              <Footer />
-            </div>
-          </GuacVizThemeContextProvider>
-        </body>
-      </html>
-    </ApolloProvider>
+    <html>
+      <body>
+        <div className="flex flex-col h-screen">
+          <Header />
+          <main className="flex-grow bg-zinc-100 dark:bg-zinc-700 text-black dark:text-zinc-50">
+            {children}
+          </main>
+          <Footer />
+        </div>
+      </body>
+    </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -11,6 +11,9 @@ import { Breadcrumb } from "@/components/breadcrumb";
 import { NavigationButtons } from "@/components/navigationButton";
 import { useBreadcrumbNavigation } from "@/hooks/useBreadcrumbNavigation";
 import QueryVuln from "@/components/queryvuln/queryVuln";
+import { GuacVizThemeContextProvider } from "@/store/themeContext";
+import { ApolloProvider } from "@apollo/client";
+import client from "@/apollo/client";
 
 export default function Home() {
   const [highlights, setHighlights] = useState({
@@ -47,82 +50,86 @@ export default function Home() {
   // load package data
   const { packageTypes, packageLoading, packageError } = usePackageData();
 
-  // // container sizing state
+  // container sizing state
   const { containerRef, size } = useContainerSize();
 
   // todo (shaf): fix re-render issue with graphData
   // console.log(graphData);
 
   return (
-    <>
-      <main className="h-full flex flex-col items-center p-12">
-        {packageLoading ? (
-          <div>Loading package types...</div>
-        ) : packageError ? (
-          <div>Error loading package types!</div>
-        ) : (
-          <PackageSelector
-            packageTypes={packageTypes}
-            setGraphData={setGraphDataWithInitial}
-            resetTypeFunc={reset}
-          />
-        )}
-        <NavigationButtons
-          backStack={backStack}
-          breadcrumb={breadcrumb}
-          currentIndex={currentIndex}
-          handleBackClick={handleBackClick}
-          handleForwardClick={handleForwardClick}
-          reset={reset}
-          userInteractedWithPath={userInteractedWithPath}
-        />
-        <Breadcrumb
-          breadcrumb={breadcrumb.map((item) => item.label)}
-          handleNodeClick={handleBreadcrumbClick}
-          currentIndex={currentIndex}
-        />
-        <div className="mt-8 grid grid-cols-none grid-rows-4 lg:grid-rows-none lg:grid-cols-4 h-full w-full gap-8 lg:gap-4">
-          <div className="flex flex-col font-mono text-sm p-4 row-span-1 lg:col-span-1">
-            <div className="my-5 text-lg">Highlight Nodes</div>
-            <p className="pb-5 pt-3 opacity-70">
-              <span className="font-bold uppercase">Tip:</span> Use click and
-              scroll to adjust graph. <br />
-              Right clicking a node displays more information.
-            </p>
-            <div className="flex flex-col justify-start gap-y-2 w-full">
-              <div>
-                <HighlightToggles
-                  highlights={highlights}
-                  setHighlights={setHighlights}
+    <div>
+      <ApolloProvider client={client}>
+        <GuacVizThemeContextProvider>
+          <main className="h-full flex flex-col items-center p-12">
+            {packageLoading ? (
+              <div>Loading package types...</div>
+            ) : packageError ? (
+              <div>Error loading package types!</div>
+            ) : (
+              <PackageSelector
+                packageTypes={packageTypes}
+                setGraphData={setGraphDataWithInitial}
+                resetTypeFunc={reset}
+              />
+            )}
+            <NavigationButtons
+              backStack={backStack}
+              breadcrumb={breadcrumb}
+              currentIndex={currentIndex}
+              handleBackClick={handleBackClick}
+              handleForwardClick={handleForwardClick}
+              reset={reset}
+              userInteractedWithPath={userInteractedWithPath}
+            />
+            <Breadcrumb
+              breadcrumb={breadcrumb.map((item) => item.label)}
+              handleNodeClick={handleBreadcrumbClick}
+              currentIndex={currentIndex}
+            />
+            <div className="mt-8 grid grid-cols-none grid-rows-4 lg:grid-rows-none lg:grid-cols-4 h-full w-full gap-8 lg:gap-4">
+              <div className="flex flex-col font-mono text-sm p-4 row-span-1 lg:col-span-1">
+                <div className="my-5 text-lg">Highlight Nodes</div>
+                <p className="pb-5 pt-3 opacity-70">
+                  <span className="font-bold uppercase">Tip:</span> Use click
+                  and scroll to adjust graph. <br />
+                  Right clicking a node displays more information.
+                </p>
+                <div className="flex flex-col justify-start gap-y-2 w-full">
+                  <div>
+                    <HighlightToggles
+                      highlights={highlights}
+                      setHighlights={setHighlights}
+                    />
+                  </div>
+                  <div>
+                    <QueryVuln />
+                  </div>
+                </div>
+              </div>
+
+              <div
+                className="lg:col-span-3 row-span-3 h-full w-full"
+                ref={containerRef}
+              >
+                <Graph
+                  graphData={graphData}
+                  onNodeClick={handleNodeClick}
+                  options={{
+                    highlightArtifact: highlights.artifact,
+                    highlightVuln: highlights.vuln,
+                    highlightSbom: highlights.sbom,
+                    highlightBuilder: highlights.builder,
+                  }}
+                  containerOptions={{
+                    width: size.width,
+                    height: size.height,
+                  }}
                 />
               </div>
-              <div>
-                <QueryVuln />
-              </div>
             </div>
-          </div>
-
-          <div
-            className="lg:col-span-3 row-span-3 h-full w-full"
-            ref={containerRef}
-          >
-            <Graph
-              graphData={graphData}
-              onNodeClick={handleNodeClick}
-              options={{
-                highlightArtifact: highlights.artifact,
-                highlightVuln: highlights.vuln,
-                highlightSbom: highlights.sbom,
-                highlightBuilder: highlights.builder,
-              }}
-              containerOptions={{
-                width: size.width,
-                height: size.height,
-              }}
-            />
-          </div>
-        </div>
-      </main>
-    </>
+          </main>
+        </GuacVizThemeContextProvider>
+      </ApolloProvider>
+    </div>
   );
 }
