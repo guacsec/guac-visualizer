4b34a7ae971799344ae73c9c998d1f167429e1a7
Martin W. Kirst <maki@bitkings.de>
2024-10-28 16:48:37 +0100

    resolves #62 (#114)
    display guacgql server version in the footer,
    as well as visualizer version in the header.
    simplify logging code for backend calls;
    send version information to guacgql backend
    
    Signed-off-by: nitram509 <maki@bitkings.de>

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -1,42 +1,36 @@
 import type {NextApiRequest, NextApiResponse} from 'next'
 import got from 'got'
 import * as console from "node:console";
-import {GUACGQL_SERVER_URL} from "@/src/config";
+import {GUACGQL_SERVER_QUERY_URL} from "@/src/config";
+import packageJson from "@/package.json";
 
 export const config = {
-    api: {
-        // Enable `externalResolver` option in Next.js
-        externalResolver: true,
-        bodyParser: false,
-    },
+  api: {
+    // Enable `externalResolver` option in Next.js
+    externalResolver: true,
+    bodyParser: false,
+  },
 }
 
 export default async function requestGuacGraphql(req: NextApiRequest, res: NextApiResponse) {
-    const options = {
-        body: req.read(),
-        timeout: {
-            connect: 5 // assuming the backend should be able to establish connection in 5 seconds
-        },
-        responseType: "buffer",
-        headers: {
-            "User-Agent": "guac-visualizer", // TODO: add version
-            "Accept": "application/json",
-            "Content-Type": "application/json"
-        }
-    };
-    try {
-        const guacgqlResponse = await got.post(GUACGQL_SERVER_URL, options);
-        return res.status(guacgqlResponse.statusCode).send(guacgqlResponse.body);
-    } catch (error) {
-        console.log("guacgql server error: " + error + ", code: " + error.code);
-        let responseBody = "internal server error";
-        if (error.name == "HTTPError") {
-            responseBody = error.response.body;
-            console.log("gguacgql server response, " +
-                "code: " + error.response.statusCode + ", " +
-                "message: " + responseBody);
-        }
-        return res.status(500).send(responseBody);
+  const options = {
+    body: req.read(),
+    timeout: {
+      connect: 5 // assuming the backend should be able to establish connection in 5 seconds
+    },
+    responseType: "buffer",
+    headers: {
+      "User-Agent": "guac-visualizer-v" + packageJson.version,
+      "Accept": "application/json",
+      "Content-Type": "application/json"
     }
+  };
+  try {
+    const guacgqlResponse = await got.post(GUACGQL_SERVER_QUERY_URL, options);
+    return res.status(guacgqlResponse.statusCode).send(guacgqlResponse.body);
+  } catch (error) {
+    console.log("requestGuacGraphql() -> guacgql server error: " + error + ", url: " + GUACGQL_SERVER_QUERY_URL + ", code: " + error.code + ", message:" + error.response?.body);
+    return res.status(500).send(error.name == "HTTPError" ? error.response.body : "internal server error");
+  }
 }
 
--- src/config.ts
+++ src/config.ts
@@ -1,14 +1,15 @@
 import console from "node:console";
 
 function getGuacQlServerUrl() {
-    let url = process.env.GUACGQL_SERVER_URL;
-    url = url.trim();
-    while (url.length > 0 && url.endsWith("/")) {
-        url = url.substring(0, url.length - 1);
-    }
-    url += "/query";
-    console.log("Using GUACGQL_SERVER_URL=" + url);
-    return new URL(url);
+  let url = process.env.GUACGQL_SERVER_URL;
+  url = url.trim();
+  while (url.length > 0 && url.endsWith("/")) {
+    url = url.substring(0, url.length - 1);
+  }
+  console.log("Using GUACGQL_SERVER_URL=" + url);
+  return url;
 }
 
-export const GUACGQL_SERVER_URL = getGuacQlServerUrl();
+const GUACGQL_SERVER_URL = getGuacQlServerUrl();
+export const GUACGQL_SERVER_QUERY_URL = new URL(GUACGQL_SERVER_URL + "/query");
+export const GUACGQL_SERVER_VERSION_URL = new URL(GUACGQL_SERVER_URL + "/version");
