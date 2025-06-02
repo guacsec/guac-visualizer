3a709833f249a953fddc476ebd626c05d41ea86c
Gagan H R <92567822+gaganhr94@users.noreply.github.com>
2025-06-03 03:39:47 +0530

    fix: wrap useSearchParam in suspense to resolve build error (#140)
    Signed-off-by: Gagan H R <hrgagan4@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useState, Suspense } from "react";
 import Graph from "@/components/graph/Graph";
 // import { HighlightToggles } from "@/components/highlightToggles";
 import { useGraphData } from "@/hooks/useGraphData";
@@ -17,7 +17,7 @@ import { PackageDataProvider } from "@/store/packageDataContext";
 import NodeInfo from "@/components/nodeInfo/nodeInfo";
 import { VulnResultsProvider } from "@/store/vulnResultsContext";
 
-export default function Home() {
+function HomeContent() {
   const [highlights, setHighlights] = useState({
     artifact: false,
     vuln: false,
@@ -54,84 +54,101 @@ export default function Home() {
   const { containerWidth, containerHeight } = useDimensions();
 
   return (
+    <main className="h-full w-screen md:w-auto flex flex-col p-10">
+      {packageLoading ? (
+        <div>Loading package types...</div>
+      ) : packageError ? (
+        <div>Error loading package types!</div>
+      ) : (
+        <div className="flex flex-col md:flex-row justify-center py-5">
+          <PackageSelector
+            packageTypes={packageTypes}
+            setGraphData={setGraphDataWithInitial}
+            resetTypeFunc={reset}
+          />
+          <div>
+            <QueryVuln />
+          </div>
+        </div>
+      )}
+
+      <div className="flex flex-col justify-center items-center lg:items-start lg:flex-row">
+        {/* TODO: Fix highlighter, until then keep it commented */}
+        {/* <div className="flex flex-col text-sm p-4 row-span-1 lg:col-span-1">
+          <p className="pb-5 pt-3 opacity-70">
+            <span className="font-bold uppercase">Tip:</span> Use click
+            and scroll to adjust graph. <br />
+            Right clicking a node displays more information.
+          </p>
+          <h1 className="my-5 text-lg">Highlight Nodes</h1>
+          <div className="flex flex-col justify-start gap-y-2 w-full">
+            <div>
+              <HighlightToggles
+                highlights={highlights}
+                setHighlights={setHighlights}
+              />
+            </div>
+
+          </div>
+        </div>  */}
+
+        <div className="p-8 lg:p-0">
+          <Graph
+            graphData={graphData}
+            onNodeClick={handleNodeClick}
+            options={{
+              highlightArtifact: highlights.artifact,
+              highlightVuln: highlights.vuln,
+              highlightSbom: highlights.sbom,
+              highlightBuilder: highlights.builder,
+            }}
+            containerOptions={{
+              width: containerWidth,
+              height: containerHeight,
+            }}
+          />
+          {graphData.nodes.length !== 0 &&
+            graphData.links.length !== 0 && (
+              <NavigationButtons
+                backStack={backStack}
+                breadcrumb={breadcrumb}
+                currentIndex={currentIndex}
+                handleBackClick={handleBackClick}
+                handleForwardClick={handleForwardClick}
+                reset={reset}
+                userInteractedWithPath={userInteractedWithPath}
+              />
+            )}
+          <Breadcrumb
+            breadcrumb={breadcrumb.map((item) => item.label)}
+            handleNodeClick={handleBreadcrumbClick}
+            currentIndex={currentIndex}
+          />
+        </div>
+        <NodeInfo />
+      </div>
+    </main>
+  );
+}
+
+// Loading fallback component
+function LoadingFallback() {
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <div className="text-lg">Loading...</div>
+    </div>
+  );
+}
+
+export default function Home() {
+  return (
     <div className="flex flex-col lg:flex-row items-center justify-between m-auto">
       <ApolloProvider client={client}>
         <VulnResultsProvider>
           <PackageDataProvider>
-            <main className="h-full w-screen md:w-auto flex flex-col p-10">
-              {packageLoading ? (
-                <div>Loading package types...</div>
-              ) : packageError ? (
-                <div>Error loading package types!</div>
-              ) : (
-                <div className="flex flex-col md:flex-row justify-center py-5">
-                  <PackageSelector
-                    packageTypes={packageTypes}
-                    setGraphData={setGraphDataWithInitial}
-                    resetTypeFunc={reset}
-                  />
-                  <div>
-                    <QueryVuln />
-                  </div>
-                </div>
-              )}
-
-              <div className="flex flex-col justify-center items-center lg:items-start lg:flex-row">
-                {/* TODO: Fix highlighter, until then keep it commented */}
-                {/* <div className="flex flex-col text-sm p-4 row-span-1 lg:col-span-1">
-                <p className="pb-5 pt-3 opacity-70">
-                  <span className="font-bold uppercase">Tip:</span> Use click
-                  and scroll to adjust graph. <br />
-                  Right clicking a node displays more information.
-                </p>
-                <h1 className="my-5 text-lg">Highlight Nodes</h1>
-                <div className="flex flex-col justify-start gap-y-2 w-full">
-                  <div>
-                    <HighlightToggles
-                      highlights={highlights}
-                      setHighlights={setHighlights}
-                    />
-                  </div>
-
-                </div>
-              </div>  */}
-
-                <div className="p-8 lg:p-0">
-                  <Graph
-                    graphData={graphData}
-                    onNodeClick={handleNodeClick}
-                    options={{
-                      highlightArtifact: highlights.artifact,
-                      highlightVuln: highlights.vuln,
-                      highlightSbom: highlights.sbom,
-                      highlightBuilder: highlights.builder,
-                    }}
-                    containerOptions={{
-                      width: containerWidth,
-                      height: containerHeight,
-                    }}
-                  />
-                  {graphData.nodes.length !== 0 &&
-                    graphData.links.length !== 0 && (
-                      <NavigationButtons
-                        backStack={backStack}
-                        breadcrumb={breadcrumb}
-                        currentIndex={currentIndex}
-                        handleBackClick={handleBackClick}
-                        handleForwardClick={handleForwardClick}
-                        reset={reset}
-                        userInteractedWithPath={userInteractedWithPath}
-                      />
-                    )}
-                  <Breadcrumb
-                    breadcrumb={breadcrumb.map((item) => item.label)}
-                    handleNodeClick={handleBreadcrumbClick}
-                    currentIndex={currentIndex}
-                  />
-                </div>
-                <NodeInfo />
-              </div>
-            </main>
+            <Suspense fallback={<LoadingFallback />}>
+              <HomeContent />
+            </Suspense>
           </PackageDataProvider>
         </VulnResultsProvider>
       </ApolloProvider>
--- next.config.js
+++ next.config.js
@@ -7,11 +7,6 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-  // FIXME: Instead of disabling this, we should fix the problem. See
-  // https://github.com/guacsec/guac-visualizer/issues/98
-  experimental: {
-    missingSuspenseWithCSRBailout: false,
-  },
   async rewrites() {
     return [
       {
